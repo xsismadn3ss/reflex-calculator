070551a5655481ada4a6f258759a638db25837d7
xsismadn3ss <2477carlos.a@gmail.com>
2024-11-11 21:13:41 -0600

    add calculator component

--- reflex_calculator/pages/index.py
+++ reflex_calculator/pages/index.py
@@ -1,9 +1,11 @@
 import reflex as rx
+from ..components.calculator.calculator import calculator
 
 
 @rx.page(route="/", title="Home", description="My first app using reflex")
 def index() -> rx.Component:
-    return rx.center(  # rx.center is a div with justify center property
-        rx.heading("Hello world"),
-        direction="column",  # direction is like flex direction on CSS
+    return rx.center(
+        calculator(),
+        direction="column",
+        margin_top='2rem'
     )
