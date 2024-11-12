449be31dbf97b3499f72252a14b9b8cb36b720f5
xsismadn3ss <2477carlos.a@gmail.com>
2024-11-11 21:21:04 -0600

    delete theme

--- reflex_calculator/reflex_calculator.py
+++ reflex_calculator/reflex_calculator.py
@@ -2,11 +2,4 @@ import reflex as rx
 from rxconfig import config
 from .pages.index import index
 
-app = rx.App(
-    app=rx.App(
-        theme=rx.theme(
-            appearance="light",
-            accent_color="orange",
-        )
-    )
-)
+app = rx.App()
