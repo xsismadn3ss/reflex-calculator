de6e031ee39fc84925312d4e569bbd178487bcb6
xsismadn3ss <2477carlos.a@gmail.com>
2024-11-11 21:13:49 -0600

    theming

--- reflex_calculator/reflex_calculator.py
+++ reflex_calculator/reflex_calculator.py
@@ -2,4 +2,11 @@ import reflex as rx
 from rxconfig import config
 from .pages.index import index
 
-app = rx.App()
+app = rx.App(
+    app=rx.App(
+        theme=rx.theme(
+            appearance="light",
+            accent_color="orange",
+        )
+    )
+)
